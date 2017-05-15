d5feb8b768dbc5d9d1375aa3dd2243d5f43a3b95
Joseph Paulo V. Los Baños <paulo.losbanos@tappcommerce.com>
2017-05-15 12:33:25 +0800

    adding dependencies. initial work in home fragment

--- app/src/main/java/com/mybareskinph/theBareskinApp/views/HomeActivity.java
+++ app/src/main/java/com/mybareskinph/theBareskinApp/views/HomeActivity.java
@@ -2,11 +2,13 @@ package com.mybareskinph.theBareskinApp.views;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.mybareskinph.theBareskinApp.R;
@@ -30,6 +32,11 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        HomeFragment fragment = new HomeFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment, "tag")
+                .commit();
+
     }
 
     @Override
@@ -45,19 +52,18 @@ public class HomeActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
 
-        if (id == R.id.nav_inventory) {
-            // Handle the camera action
-        } else if (id == R.id.nav_home) {
-
-        } else if (id == R.id.nav_order) {
-
-        } else if (id == R.id.nav_about) {
-
-        } else if (id == R.id.nav_log_out) {
-
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_inventory:
+                break;
+            case R.id.nav_order:
+                break;
+            case R.id.nav_about:
+                break;
+            case R.id.nav_log_out:
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
--- app/src/main/java/com/mybareskinph/theBareskinApp/views/HomeFragment.java
+++ app/src/main/java/com/mybareskinph/theBareskinApp/views/HomeFragment.java
@@ -1,8 +1,20 @@
 package com.mybareskinph.theBareskinApp.views;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mybareskinph.theBareskinApp.R;
 
 public class HomeFragment extends Fragment{
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_layout_home, container, false);
+        return view;
+    }
 
 }
